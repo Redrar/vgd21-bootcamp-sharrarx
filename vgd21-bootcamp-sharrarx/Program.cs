e50c84a310b9628a2b1a5da50f62f265b16f81a4
Redrar <redra100@gmail.com>
2021-02-01 08:59:17 -0800

    Added PAK to exit.

--- vgd21-bootcamp-sharrarx/Program.cs
+++ vgd21-bootcamp-sharrarx/Program.cs
@@ -7,7 +7,9 @@ namespace vgd21_bootcamp_sharrarx
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Any Key to Exit.");
             Console.ReadKey();
+            
         }
     }
 }
